9c3f15ba7e1c3686ea5c203d675c7876d0268fe3
batuhan34 <bfindik@ekofactoring.com>
2022-06-30 17:29:03 +0300

    .

--- TekrarApp/Controllers/UserController.cs
+++ TekrarApp/Controllers/UserController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using TekrarApp.Model;
 
 namespace TekrarApp.Controllers
@@ -27,7 +32,7 @@ namespace TekrarApp.Controllers
         }
 
         [HttpPost("[action]")]
-        public async Task<ActionResult<Token>> Login([FromForm] UserLogin userLogin)
+        public async Task<ActionResult<Token>> Login( UserLogin userLogin)
         {
             User user = await context.Users.Include(u => u.UserRoles).ThenInclude(ur => ur.Role).FirstOrDefaultAsync(x=>x.Email == userLogin.Email && x.Password == userLogin.Password);
             
@@ -38,6 +43,95 @@ namespace TekrarApp.Controllers
                 user.RefreshToken = token.RefreshToken;
                 user.RefrestTokenEndDate = token.Expiration.AddMinutes(3);
                 await context.SaveChangesAsync();
+
+                //var claims = new List<Claim>()
+                //{
+                //    new Claim("AccessToken",token.AccessToken),
+                //    new Claim("RefreshToken",token.RefreshToken)
+                //};
+                //var userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                //ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                //await HttpContext.SignInAsync(principal);
+
+
+                var cookieOptions = new CookieOptions
+                {
+                    //Expires = DateTime.UtcNow.AddHours(8),
+                    //Path = "/login",
+                };
+                Response.Cookies.Append("RefreshToken", token.RefreshToken, cookieOptions);
+   
+                //Response.Cookies.Append(key:"jwt",value:"jwy",new CookieOptions { HttpOnly=true});
+                //HttpContext.Response.Cookies.Append("AccessToken", token.AccessToken, cookieOptions);
+
+                //var claims = new List<Claim>
+                //    {
+                //       new Claim("AccessToken",token.AccessToken),
+                //       new Claim("RefreshToken",token.RefreshToken)
+                //    };
+                //var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                //var authenticationProperty = new AuthenticationProperties
+                //{
+                //    RedirectUri = @"/public/login"
+                //};
+
+
+                //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme
+                //                        , new ClaimsPrincipal(claimIdentity)
+                //                        , authenticationProperty);
+
+
+
+                //        var claims = new List<Claim>
+                //{
+                //    new Claim(ClaimTypes.Name, user.Email),
+                //    new Claim("Token", user.RefreshToken),
+                //    new Claim(ClaimTypes.Role, "Administrator"),
+                //};
+
+                //        var claimsIdentity = new ClaimsIdentity(
+                //            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                //        var authProperties = new AuthenticationProperties
+                //        {
+                //            //AllowRefresh = <bool>,
+                //            // Refreshing the authentication session should be allowed.
+
+                //            //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                //            // The time at which the authentication ticket expires. A 
+                //            // value set here overrides the ExpireTimeSpan option of 
+                //            // CookieAuthenticationOptions set with AddCookie.
+
+                //            IsPersistent = true,
+                //            // Whether the authentication session is persisted across 
+                //            // multiple requests. When used with cookies, controls
+                //            // whether the cookie's lifetime is absolute (matching the
+                //            // lifetime of the authentication ticket) or session-based.
+
+                //            IssuedUtc = DateTime.Now,
+                //            // The time at which the authentication ticket was issued.
+
+                //            RedirectUri = "http://localhost:3000/login"
+                //            // The full path or absolute URI to be used as an http 
+                //            // redirect response value.
+                //        };
+
+                //        await HttpContext.SignInAsync(
+                //            CookieAuthenticationDefaults.AuthenticationScheme,
+                //            new ClaimsPrincipal(claimsIdentity),
+                //            authProperties);
+
+
+                //SHA1 sha = new SHA1CryptoServiceProvider();
+
+
+
+                //string HashAccessToken = Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(token.AccessToken)));
+                //string HashRefreshToken = Convert.ToBase64String(sha.ComputeHash(Encoding.UTF8.GetBytes(token.RefreshToken)));
+                //token.AccessToken = HashAccessToken;
+                //token.RefreshToken = HashRefreshToken;
+
+
                 return Ok(token);
             }
 
--- TekrarApp/Program.cs
+++ TekrarApp/Program.cs
@@ -1,6 +1,8 @@
 using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 using TekrarApp.Model;
 
@@ -12,12 +14,15 @@ ConfigurationManager Configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<JwtDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("JwtConStr")));
-builder.Services.AddCors(options =>
-                        options.AddDefaultPolicy(builder =>
-                        builder.AllowAnyHeader()
-                        .AllowAnyMethod()
-                        .AllowAnyOrigin())
-                        );
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => {
+    policy.WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200", "http://localhost:7024" })
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+                .AllowCredentials();                   
+}));
+
+
 var key = Encoding.ASCII.GetBytes(Configuration["Token:SecurityKey"]);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -37,6 +42,41 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(x =>
+//{
+//    x.LoginPath = "/login#";
+//    x.LogoutPath = "/public/login";
+//    x.AccessDeniedPath = "/public/login";
+//    x.Cookie.HttpOnly = false;
+//    x.Cookie.Name = "TokenUserCookie";
+//    x.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+//    x.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+//    x.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+
+//});
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+//    .AddCookie(options =>
+//    {
+//        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+//        options.SlidingExpiration = true;
+//        options.AccessDeniedPath = "/Forbidden/";
+//        options.LoginPath = "/login";
+//        options.LogoutPath = "/index.html";
+//        options.Cookie.HttpOnly = false;
+//        options.Cookie.Name = "TokenUserCookie";
+//        options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict;
+//        options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+//        options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
+//    });
+
+
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+//{
+//    options.LoginPath = @"/http://localhost:3000/login#";
+//});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -55,5 +95,12 @@ app.UseAuthorization();
 app.UseCors();
 
 app.MapControllers();
+
+//app.UseStaticFiles(new StaticFileOptions
+//{
+//    FileProvider = new PhysicalFileProvider(
+//        Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
+//    RequestPath = "C:/Users/bfindik/Desktop"
+//});
 
 app.Run();
